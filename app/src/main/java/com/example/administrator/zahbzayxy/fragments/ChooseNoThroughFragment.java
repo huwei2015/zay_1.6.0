0ba80ee9d4f385d7485ef06b060880471a575e49
huwei <12345678>
2020-01-15 17:03:32 +0800

    添加考试过期空白页面

--- app/src/main/java/com/example/administrator/zahbzayxy/fragments/ChooseNoThroughFragment.java
+++ app/src/main/java/com/example/administrator/zahbzayxy/fragments/ChooseNoThroughFragment.java
@@ -3,6 +3,7 @@ package com.example.administrator.zahbzayxy.fragments;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,6 +12,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.androidkun.PullToRefreshRecyclerView;
@@ -19,6 +23,7 @@ import com.example.administrator.zahbzayxy.R;
 import com.example.administrator.zahbzayxy.adapters.NotThrougAdapter;
 import com.example.administrator.zahbzayxy.beans.NotThroughBean;
 import com.example.administrator.zahbzayxy.interfacecommit.UserInfoInterface;
+import com.example.administrator.zahbzayxy.utils.ProgressBarLayout;
 import com.example.administrator.zahbzayxy.utils.RetrofitUtils;
 import com.example.administrator.zahbzayxy.utils.ToastUtils;
 import com.example.administrator.zahbzayxy.vo.UserInfo;
@@ -44,6 +49,10 @@ public class ChooseNoThroughFragment extends Fragment implements PullToRefreshLi
     private String token;
     private int  currenPage =1;
     private int pageSize =10;
+    private RelativeLayout rl_empty;
+    TextView tv_msg;
+    LinearLayout ll_list;
+    private ProgressBarLayout mLoadingBar;
     private List<NotThroughBean.THrougListData> notPassListBeans = new ArrayList<>();
 
     @Nullable
@@ -62,23 +71,29 @@ public class ChooseNoThroughFragment extends Fragment implements PullToRefreshLi
     }
 
     private void initData(){
+        showLoadingBar(false);
         UserInfoInterface userInfoInterface = RetrofitUtils.getInstance().createClass(UserInfoInterface.class);
         userInfoInterface.getQuestionData(currenPage,pageSize,301,"0",token).enqueue(new Callback<NotThroughBean>() {
             @Override
             public void onResponse(Call<NotThroughBean> call, Response<NotThroughBean> response) {
                         if(response !=null && response.body() !=null){
                             String code = response.body().getCode();
-                            if(code.equals("00000")){
+                            if(code.equals("00000") && response.body().getData().getqLibs().getData().size() > 0){
+                                emptyLayout(true);
                                 List<NotThroughBean.THrougListData> data = response.body().getData().getqLibs().getData();
                                 notPassListBeans.addAll(data);
                                througAdapter.setList(notPassListBeans);
 
+                            }else{
+                                hideLoadingBar();
+                                emptyLayout(false);
                             }
                         }
             }
 
             @Override
             public void onFailure(Call<NotThroughBean> call, Throwable t) {
+                hideLoadingBar();
                 ToastUtils.showInfo(t.getMessage(),5000);
             }
         });
@@ -87,6 +102,10 @@ public class ChooseNoThroughFragment extends Fragment implements PullToRefreshLi
         SharedPreferences sharedPreferences =mContext.getSharedPreferences("tokenDb", mContext.MODE_PRIVATE);
         token = sharedPreferences.getString("token", "");
         pullToRefreshRecyclerView=view.findViewById(R.id.pull_recycleview);
+        rl_empty= view.findViewById(R.id.rl_empty_layout);//空页面
+        ll_list = view.findViewById(R.id.ll_list);
+        tv_msg = view.findViewById(R.id.tv_msg);
+        mLoadingBar = view.findViewById(R.id.nb_allOrder_load_bar_layout);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         //初始化adapter
@@ -127,5 +146,23 @@ public class ChooseNoThroughFragment extends Fragment implements PullToRefreshLi
     @Override
     public void onItemClick(View view, int position) {
         Toast.makeText(getActivity(),"点击了"+position,Toast.LENGTH_LONG).show();
+    }
+    private void emptyLayout(boolean isVisible){
+        if(isVisible){
+            pullToRefreshRecyclerView.setVisibility(View.VISIBLE);
+            rl_empty.setVisibility(View.GONE);
+        }else{
+            rl_empty.setVisibility(View.VISIBLE);
+            pullToRefreshRecyclerView.setVisibility(View.GONE);
+            tv_msg.setText("暂无未通过数据");
+        }
+    }
+    public void showLoadingBar(boolean transparent) {
+        mLoadingBar.setBackgroundColor(transparent ? Color.TRANSPARENT : getResources().getColor(R.color.main_bg));
+        mLoadingBar.show();
+    }
+
+    public void hideLoadingBar() {
+        mLoadingBar.hide();
     }
 }
